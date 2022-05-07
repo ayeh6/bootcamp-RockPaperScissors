97c434a2ed18ca088e548dde9c933f15cd7860cb
Andrew Yeh <ayeh2317@gmail.com>
2022-05-07 13:54:27 -0700

    change var to let

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var playerWins = 0;
-var compWins = 0;
+let playerWins = 0;
+let compWins = 0;
 
 function rockPaperScissors() {
-    var playerChoice = prompt("Pick Rock, Paper or Scissors using R, P or S");
+    let playerChoice = prompt("Pick Rock, Paper or Scissors using R, P or S");
     //check valid input
     if(playerChoice === 'R' || playerChoice === 'P' || playerChoice === 'S') {
         alert(`You chose ${playerChoice}`);
@@ -12,11 +12,11 @@ function rockPaperScissors() {
         return;
     }
     //choice array for computer
-    var choices = ['R', 'P', 'S'];
+    let choices = ['R', 'P', 'S'];
     //pick random choice for computer
-    var compChoice = choices[Math.floor(Math.random()*3)];      // Math.floor(Math.random()*3) returns a random integer from 0-2
+    let compChoice = choices[Math.floor(Math.random()*3)];      // Math.floor(Math.random()*3) returns a random integer from 0-2
     alert(`Computer chose ${compChoice}`);
-    var resultCheck = checkWinner(playerChoice, compChoice);
+    let resultCheck = checkWinner(playerChoice, compChoice);
     if(resultCheck !== undefined) {
         if(resultCheck) {
             //increment player wins, update the <p> object
